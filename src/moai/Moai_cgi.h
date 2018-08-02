0860aaf6afabef57ce9e9b8d8c1cc327e787e62e
Mr.Moai <mr_moai_2016@hotmail.com>
2018-08-02 23:46:59 +0900

    Ver2.1 release draft

--- src/moai/Moai_cgi.h
+++ src/moai/Moai_cgi.h
@@ -18,6 +18,9 @@ MoaiCGI_runPost( const char* cmd, const char* curdir_new, ZnkSocket sock, RanoMo
 		const size_t hdr_size, ZnkStrAry hdr1st, ZnkVarpAry hdr_vars,
 		size_t content_length, ZnkBfr stream, ZnkStr query_str, bool is_xhr_dmz );
 
+void
+MoaiCGIManager_makeHeader( ZnkStr html, const char* title, bool inline_script );
+
 bool
 MoaiCGIManager_load( void );
 bool
@@ -37,13 +40,6 @@ bool
 MoaiCGIManager_isValidReqUrp_forRun( const char* req_urp );
 bool
 MoaiCGIManager_isValidReqUrp_forPathBegin( const char* req_urp, const char* path_list_name );
-
-#if 0
-bool
-MoaiCGIManager_isAuthenticReqUrp( const char* req_urp );
-bool
-MoaiCGIManager_isValidReqUrp_forBox( const char* req_urp, const char* box_list_name );
-#endif
 
 /**
  * @note
